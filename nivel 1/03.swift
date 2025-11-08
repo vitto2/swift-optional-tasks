09f731597a2baf9750a22808645810ad8d01cd38
vitto2 <vitor2071@gmail.com>
2025-11-08 19:58:55 -0300

    feat: implementa desafio 03 nivel 01

---
+++ nivel 1/03.swift
@@ -0,0 +1,10 @@
+//Crie uma variável String? com valor nil. Use o Nil-Coalescing Operator (??) para atribuir a ela o valor padrão "Convidado" em uma nova constante nomeDisplay. Imprima nomeDisplay
+
+var str: String? = nil 
+
+let nomeDisplay: String = str ?? "convidado"
+
+
+print(nomeDisplay)
+
+
